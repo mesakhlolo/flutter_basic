214d01d7fca52d17d47d79d87dd2e57dc478d9ed
Mesakh Lolo <mesakhlolo12@gmail.com>
2024-03-18 22:05:07 +0700

    navigator

--- lib/main.dart
+++ lib/main.dart
@@ -19,64 +19,47 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('App Title'),
-        ),
-        body: Center(
-          child: currentIndex == 0
-              ? Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  color: Colors.red,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          foregroundColor: Colors.black,
-                          backgroundColor: Colors.orange,
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            buttonName = 'Mantap cuy';
-                          });
-                        },
-                        child: Text(buttonName),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          setState(() {
-                            buttonName = 'Mantap cuy';
-                          });
-                        },
-                        child: Text(buttonName),
-                      ),
-                    ],
-                  ),
-                )
-              : Image.asset('images/spiderman.jpg'),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              label: 'Home',
-              icon: Icon(Icons.home),
-            ),
-            BottomNavigationBarItem(
-              label: 'Settings',
-              icon: Icon(Icons.settings),
-            )
-          ],
-          currentIndex: currentIndex,
-          onTap: (int index) {
-            setState(() {
-              currentIndex = index;
-            });
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FirstPage(),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(builder: (BuildContext context) {
+                return const SecondPage();
+              }),
+            );
           },
+          child: const Text('Press'),
         ),
       ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
     );
   }
 }
