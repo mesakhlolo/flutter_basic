f1593dbffd202da01fbb71127720bd18d79fe9dd
Mesakh Lolo <mesakhlolo12@gmail.com>
2024-03-20 23:56:10 +0700

    split codes

---
+++ lib/next_page/next_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import '../main.dart';
+
+class NextPage extends StatelessWidget {
+  const NextPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
